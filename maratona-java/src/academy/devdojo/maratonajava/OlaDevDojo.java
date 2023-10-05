aca13168499d8b5a20064a831725d0fb998c7067
unluckdev <unluckdev@gmail.com>
2023-10-04 22:29:49 -0300

    Update OlaDevDojo.java

--- maratona-java/src/academy/devdojo/maratonajava/OlaDevDojo.java
+++ maratona-java/src/academy/devdojo/maratonajava/OlaDevDojo.java
@@ -1,7 +1,8 @@
 package academy.devdojo.maratonajava;
 
 /**
- * Isso é um comentário <b>Javadoc</b>
+ * Isso é um comentário <b>Javadoc</b> <br>
+ * Métodos públicos devem ter um comentário <b>Javadoc</b>
  */
 public class OlaDevDojo {
     public static void main(String[] args) {
