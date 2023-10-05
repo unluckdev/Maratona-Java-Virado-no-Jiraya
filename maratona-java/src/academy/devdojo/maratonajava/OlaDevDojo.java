56eace2f3da4a3a7cc57b98c13152bf751354c46
unluckdev <unluckdev@gmail.com>
2023-10-04 22:26:37 -0300

    Comentário
    Javadoc

--- maratona-java/src/academy/devdojo/maratonajava/OlaDevDojo.java
+++ maratona-java/src/academy/devdojo/maratonajava/OlaDevDojo.java
@@ -1,5 +1,8 @@
 package academy.devdojo.maratonajava;
 
+/**
+ * Isso é um comentário <b>Javadoc</b>
+ */
 public class OlaDevDojo {
     public static void main(String[] args) {
         System.out.println("Olá mundo JAVA!");
