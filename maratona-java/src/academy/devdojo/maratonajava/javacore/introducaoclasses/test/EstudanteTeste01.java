15e4e4939c1a6829ed4b51f1f85455bbdc64a6ad
unluckdev <unluckdev@gmail.com>
2023-10-12 19:10:51 -0300

    novaspastas

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTeste01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        //Variavel de referencia do tipo Estudante
+        Estudante estudante = new Estudante();
+        estudante.i
+    }
+}
