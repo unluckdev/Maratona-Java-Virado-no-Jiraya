889d7103611b659c57fce05d5b6c69688997b4e8
unluckdev <unluckdev@gmail.com>
2023-10-13 18:07:26 -0300

    Aula42

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Carro.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+//Crie uma classe carro com os seguintes atributos
+// marca, modelo, ano
+// Em seguida, crie dois objetos distintos e impra seus valores
+public class Carro {
+    public String marca;
+    public String modelo;
+    public int ano;
+
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTeste01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTeste01 {
+    public static void main(String[] args) {
+        Carro carro01 = new Carro();
+        Carro carro02 = new Carro();
+
+        carro01.marca = "Dodge";
+        carro01.modelo = "Charger";
+        carro01.ano = 1970;
+
+        carro02.marca = "Chevrolet";
+        carro02.modelo = "Impala";
+        carro02.ano = 1964;
+
+        System.out.println("Carro: " + carro01.marca + " Modelo: " + carro01.modelo + " Ano: " + carro01.ano);
+        System.out.println("-----------------");
+        System.out.println("Carro: " + carro02.marca + " Modelo: " + carro02.modelo + " Ano: " + carro02.ano);
+
+    }
+}
