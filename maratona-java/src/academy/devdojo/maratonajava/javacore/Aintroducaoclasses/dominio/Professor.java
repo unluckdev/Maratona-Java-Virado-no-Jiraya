c52282e8dc509c520c4a64cb1ad6a22e2450ef44
unluckdev <unluckdev@gmail.com>
2023-10-12 21:38:00 -0300

    Aula42
    Coesão

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Professor.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Professor.java
@@ -1,7 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
 
-public class Estudante {
+public class Professor {
     public String nome;
     public int idade;
     public char sexo;
+    public String materia;
 }
